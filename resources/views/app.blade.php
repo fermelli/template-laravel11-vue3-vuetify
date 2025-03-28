94517141961eef954d3f5cc62025072d6305270e
fermelli <luisfernandosalgadomiguez@gmail.com>
2025-03-28 18:02:51 -0400

    fix: actualizar el favicon de la aplicacion

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="shortcut icon" href="{{ asset('vue.svg') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <script>
         document.addEventListener('DOMContentLoaded', () => {
