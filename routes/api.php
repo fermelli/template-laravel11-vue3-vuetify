0793a65dc9c3431823d586f2a20033d730f8cb7d
fermelli <luisfernandosalgadomiguez@gmail.com>
2025-05-13 23:51:07 -0400

    refactor: simplificar el uso del controlador en las rutas de restablecimiento de contrasena

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomNewPasswordController;
 use App\Http\Controllers\CustomPasswordResetLinkController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
@@ -12,5 +13,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
 Route::post('/forgot-password', [CustomPasswordResetLinkController::class, 'store'])
     ->name('password.email');
 
-Route::post('/reset-password', [\App\Http\Controllers\CustomNewPasswordController::class, 'store'])
+Route::post('/reset-password', [CustomNewPasswordController::class, 'store'])
     ->name('password.update');
